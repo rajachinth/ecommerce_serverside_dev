3d0f1918048501d01314d256f4448a17f0f0923f
rajachinth <rajasekhar9040@outlook.com>
2020-06-01 18:33:37 +0530

    V1.0.0

--- startup/nodeconfig.js
+++ startup/nodeconfig.js
@@ -7,8 +7,9 @@ const pug=require('pug');
 module.exports=function nodeConfig(app)
 {
     if(!config.get('secretKey')) { process.exit(1); }
-    const PORT=process.env.HOST || 4000;
-    debug(`port ${PORT} is logged`);
+    const PORT=process.env.PORT || 4000;
+    const HOST=process.env.HOST || 4000;
+    debug(`port ${HOST} is logged`);
     const ENV=process.env.NODE_ENV || 'production';
     debug(`environment set to ${config.util.getEnv('NODE_ENV')}`);
     debug(`${config.get('name')} is active`);
