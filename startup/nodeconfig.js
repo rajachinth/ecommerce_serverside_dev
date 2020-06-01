e1c9ffcfd36466cea0a0f370aa33f37e5c69600e
rajachinth <rajasekhar9040@outlook.com>
2020-06-01 18:59:58 +0530

    V1.0.0

--- startup/nodeconfig.js
+++ startup/nodeconfig.js
@@ -9,7 +9,7 @@ module.exports=function nodeConfig(app)
     if(!config.get('secretKey')) { process.exit(1); }
     const PORT=process.env.PORT || 4000;
     const HOST=process.env.HOST || 4000;//cant bind in HEROKU, use PORT
-    debug(`port ${HOST} is logged`);
+    debug(`port ${process.env.PORT} is logged`);
     const ENV=process.env.NODE_ENV || 'production';
     debug(`environment set to ${config.util.getEnv('NODE_ENV')}`);
     debug(`${config.get('name')} is active`);
