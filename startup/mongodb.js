7a3e235973f9ff97f65a0be3bb6179bf3fa23592
rajachinth <rajasekhar9040@outlook.com>
2020-06-01 13:17:12 +0530

    V1.0.0

--- startup/mongodb.js
+++ startup/mongodb.js
@@ -4,7 +4,8 @@ const config=require('config');
 
 module.exports=function()
 {
-    mongoose.connect(config.get('db'))
+    console.log(config.get('db'));
+    mongoose.connect(config.get('db'),{useNewUrlParser:true,useUnifiedTopology:true})
           .then(()=>{debug('database connection successful')})
           .catch(()=>{debug('failed to connect database, check mongoDB local server')});
 } 
